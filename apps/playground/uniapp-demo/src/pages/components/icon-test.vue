5f47e3571bac0724f56a1d7b3f0bb09ec7c76ba4
Phill.Lv <Phill.Lv@fanruan.com>
2026-03-22 18:04:56 +0800

    feat(uniapp-demo): 配置图标组件自动导入并更新使用方式
    在 pages.json 的 easycom.custom 中添加图标组件的自动导入规则，从而无需在页面中手动导入。同时，将 icon-test.vue 中的图标组件标签从 PascalCase 更新为 kebab-case 以符合自动导入的使用规范。

--- apps/playground/uniapp-demo/src/pages/components/icon-test.vue
+++ apps/playground/uniapp-demo/src/pages/components/icon-test.vue
@@ -3,24 +3,20 @@
     <view class="section">
       <text class="title">1. 使用 Icon 组件</text>
       <view class="row">
-        <IconCalendar size="24px" />
+        <icon-calendar size="24px" />
         <text>IconCalendar</text>
       </view>
       <view class="row">
-        <IconFileFold size="28px" />
+        <icon-file-fold size="28px" />
         <text>IconFileFold</text>
       </view>
       <view class="row">
-        <IconSetting size="28px" />
+        <icon-setting size="28px" />
         <text>IconSetting</text>
       </view>
     </view>
   </view>
 </template>
-
-<script setup>
-import { IconCalendar, IconFileFold, IconSetting } from '@/uni_modules/@phill-component/icons/mobile/vue/index.js';
-</script>
 
 <style lang="scss" scoped>
 .container {
